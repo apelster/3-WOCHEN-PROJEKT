1ba5f2a6d7d1103a1121c4b8ff95f14ab3cfb099
Secret6230 <akinkale054@hotmail.com>
2024-07-03 12:13:07 +0200

    Hilf mir Hubertus

--- freundebuch/src/App.jsx
+++ freundebuch/src/App.jsx
@@ -1,20 +1,26 @@
-import React from 'react';
-import './App.css';
-import './styles.css'
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import Datenschutz from './components/Datenschutz/Datenschutz.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import "./styles.css";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import Datenschutz from "./components/Datenschutz/Datenschutz.jsx";
 
 function App() {
-    return (
-        <div className="App">
-            <Header />
-            <MainContent />
-            <Footer />
-            <link rel="stylesheet" href="./components/Datenschutz/Datenschutz" /><Datenschutz />
-            <Datenschutz />
-        </div>
-    );
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<MainContent />} />
+
+        <Route path="/datenschutz" element={<Datenschutz />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
+
 export default App;
--- freundebuch/src/components/Footer.jsx
+++ freundebuch/src/components/Footer.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import ReactDOM from "react-dom/client"
+import { Outlet, Link} from "react-router-dom";
+
 import './Footer.css';
 // import './Impressum/Impressum.jsx'
 import './Datenschutz/Datenschutz.jsx'
+
+// ich brauch einen react router
+
 
 
 
@@ -13,11 +19,12 @@ return (
         <div>
             <nav>
                 <ol>
-                    
-                <li><a href="/Impressum" className="nav-button"></a></li>
+
+                <Link to="/Datenschutz">Datenschutz</Link>
+
+                {/* <li><a href="/Impressum" className="nav-button"></a></li>
                 <li><a href="/AGB" className="nav-button">AGB</a></li>
-                <li><a href="/Datenschutz" className="nav-button">Datenschutz</a></li>
-                <li><a href="/Kontakt" className="nav-button">Kontakt</a></li>
+                <li><a href="/Kontakt" className="nav-button">Kontakt</a></li> */}
 
                 </ol>
             </nav>
