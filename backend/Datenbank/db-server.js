a03e4f05618114bccee1ee4ab463b54867c39f0c
Chris Breunig <chrisbreunig2311@gmail.com>
2024-07-12 17:07:21 +0200

    datenbank schema weiter angepasst

--- backend/Datenbank/db-server.js
+++ backend/Datenbank/db-server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = 3306;
+const port = 3001;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -43,10 +43,6 @@ app.get('/random-questions', (req, res) => {
     SELECT * FROM (
       SELECT frage FROM Fragen_4 ORDER BY RAND() LIMIT 6
     ) AS t4
-    UNION ALL
-    SELECT * FROM (
-      SELECT frage FROM Fragen_5 ORDER BY RAND() LIMIT 6
-    ) AS t5
   `;
   db.query(query, (err, results) => {
     if (err) {
@@ -63,10 +59,9 @@ app.post('/antworten', (req, res) => {
   const { profil_id, antworten } = req.body;
   const queries = [];
 
-  antworten.forEach((antwort, index) => {
-    const tableIndex = Math.floor(index / 5) + 1;
+  antworten.forEach((antwort) => {
     const query = `
-      INSERT INTO Fragen_${tableIndex} (profil_id, frage, antwort)
+      INSERT INTO Antworten (profil_id, frage, antwort)
       VALUES (?, ?, ?)
       ON DUPLICATE KEY UPDATE antwort = VALUES(antwort)
     `;
@@ -92,17 +87,9 @@ app.post('/antworten', (req, res) => {
 app.get('/antworten/:profil_id', (req, res) => {
   const { profil_id } = req.params;
   const query = `
-    SELECT * FROM Fragen_1 WHERE profil_id = ?
-    UNION
-    SELECT * FROM Fragen_2 WHERE profil_id = ?
-    UNION
-    SELECT * FROM Fragen_3 WHERE profil_id = ?
-    UNION
-    SELECT * FROM Fragen_4 WHERE profil_id = ?
-    UNION
-    SELECT * FROM Fragen_5 WHERE profil_id = ?
+    SELECT frage, antwort FROM Antworten WHERE profil_id = ?
   `;
-  db.query(query, [profil_id, profil_id, profil_id, profil_id, profil_id], (err, results) => {
+  db.query(query, [profil_id], (err, results) => {
     if (err) {
       console.error('Error fetching answers:', err);
       res.status(500).json({ error: 'Error fetching answers' });
