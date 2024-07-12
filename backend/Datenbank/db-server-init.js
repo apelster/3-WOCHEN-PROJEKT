7b33437fbee766bf61d9660a0d23b88750bc8920
Chris Breunig <chrisbreunig2311@gmail.com>
2024-07-12 12:06:29 +0200

    neu

--- backend/Datenbank/db-server-init.js
+++ backend/Datenbank/db-server-init.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2/promise');
 
 async function initializeDatabase() {
     const connection = await mysql.createConnection({
-        host: 'your-rds-endpoint', // Ersetze 'your-rds-endpoint' durch deinen RDS-Endpunkt
-        user: 'your-rds-username', // Ersetze 'your-rds-username' durch deinen RDS-Benutzernamen
-        password: 'your-rds-password', // Ersetze 'your-rds-password' durch dein RDS-Passwort
+        host: 'freundebuch.cfseo6ieksme.eu-central-1.rds.amazonaws.com', // Ersetze 'your-rds-endpoint' durch deinen RDS-Endpunkt
+        user: 'root', // Ersetze 'your-rds-username' durch deinen RDS-Benutzernamen
+        password: 'Eisbombe11#', // Ersetze 'your-rds-password' durch dein RDS-Passwort
     });
 
     // SQL Skript zur Initialisierung der Datenbank und Tabellen
