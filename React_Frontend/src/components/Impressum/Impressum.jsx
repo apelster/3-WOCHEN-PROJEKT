0d8dce71e83ed8337e8feef8c40690667a143158
Buxtehude <165007493+aiyoub2075@users.noreply.github.com>
2024-07-15 09:10:33 +0200

    Update Impressum.jsx

--- React_Frontend/src/components/Impressum/Impressum.jsx
+++ React_Frontend/src/components/Impressum/Impressum.jsx
@@ -30,7 +30,7 @@ const Impressum = () => {
         <p className='pI'>
           Kontakt:<br />
           Telefon: 1234/56789 <br />
-          E-Mail: Max.Mustermann1785@gmail.com
+          E-Mail: teamforceone2024@web.de
         </p>
        
         <p className='pI3'> 
