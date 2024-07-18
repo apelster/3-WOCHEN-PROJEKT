1c0a0e5210c613f2d5b639ea8ff444215c7fd743
Chris Breunig <chrisbreunig2311@gmail.com>
2024-07-18 23:33:42 +0200

    neu

--- React_Frontend/src/components/Freundebuch/1-Freunde.jsx
+++ React_Frontend/src/components/Freundebuch/1-Freunde.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom"; // Import useNavigate and useLocation
 import "../Profil/Profil-Design.css";
 import "./Freunde-1.css";
 
